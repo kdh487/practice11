6c1617500b5378c6d89874bd3b286167ce990f5b
daechun <kdh487@gmail.com>
2016-10-05 16:52:21 +0900

    커밋

---
+++ src/main/java/kr/co/mlec/repository/vo/Board.java
@@ -0,0 +1,27 @@
+package kr.co.mlec.repository.vo;
+
+public class Board {
+	private int no;
+	private String title;
+	private String writer;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+	
+}
