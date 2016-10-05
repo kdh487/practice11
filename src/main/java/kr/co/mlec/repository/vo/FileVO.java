b8e246066d37f34d079fb35ef3f8a6944972a103
daechun <kdh487@gmail.com>
2016-10-05 17:15:12 +0900

    호

---
+++ src/main/java/kr/co/mlec/repository/vo/FileVO.java
@@ -0,0 +1,21 @@
+package kr.co.mlec.repository.vo;
+
+public class FileVO {
+	private int no;
+	private String file;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	
+	
+	
+}
