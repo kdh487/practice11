b2f2aac7c22b114f26a477d80272ccc8d2e2bd90
daechun <kdh487@gmail.com>
2016-10-05 16:54:24 +0900

    브랜치가커밋

---
+++ src/main/java/kr/co/mlec/repository/vo/File.java
@@ -0,0 +1,20 @@
+package kr.co.mlec.repository.vo;
+
+public class File {
+	private int no;
+	private int fileNo;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getFileNo() {
+		return fileNo;
+	}
+	public void setFileNo(int fileNo) {
+		this.fileNo = fileNo;
+	}
+	
+	
+}
